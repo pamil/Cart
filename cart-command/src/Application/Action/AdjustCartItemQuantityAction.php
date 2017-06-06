d24f4e752c3b621fdbfac5cb468335520e5bc97e
Kamil Kokot <kamil@kokot.me>
2017-06-06 13:53:09 +0200

    Implement adjust cart item quantity API endpoint

---
+++ cart-command/src/Application/Action/AdjustCartItemQuantityAction.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pamil\CartCommand\Application\Action;
+
+use Broadway\CommandHandling\CommandBus;
+use Pamil\CartCommand\Application\Command\AddCartItem;
+use Pamil\CartCommand\Application\Command\AdjustCartItemQuantity;
+use Pamil\CartCommand\Application\Exception\CartNotFoundException;
+use Pamil\CartCommand\Domain\Exception\CartItemNotFoundException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class AdjustCartItemQuantityAction
+{
+    /** @var CommandBus */
+    private $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function __invoke(Request $request, string $cartId): Response
+    {
+        $content = json_decode($request->getContent(), true);
+
+        if (!isset($content['cartItemId'], $content['quantity'])) {
+            return new JsonResponse(['error' => 'Invalid request content'], 400);
+        }
+
+        try {
+            $this->commandBus->dispatch(new AdjustCartItemQuantity($cartId, $content['cartItemId'], $content['quantity']));
+
+            return new JsonResponse(null, 204);
+        } catch (CartNotFoundException $exception) {
+            return new JsonResponse(['error' => 'Cart has not been picked up before'], 404);
+        }
+    }
+}
