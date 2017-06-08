392146aeb98722722b32112d7f6c43cccee47170
Kamil Kokot <kamil@kokot.me>
2017-06-09 01:27:47 +0200

    Fix front controllers after messing with namespaces

--- cart/web/index.php
+++ cart/web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Pamil\Cart\Infrastructure\PamilCartKernel;
+use Pamil\Cart\Common\Infrastructure\PamilCartKernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
